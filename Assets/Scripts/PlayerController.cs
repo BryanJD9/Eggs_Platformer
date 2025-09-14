59562fd6cb8acc8927e82bc5dbee5469160f2cb7
BryanJD9 <156015169+BryanJD9@users.noreply.github.com>
2025-09-13 20:42:05 -0400

    Basic movement implemented
    Left/Right/Jump works
    GameObject rotation freezed, no rigidbody rotation if it collides weird

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private PlayerControls controls;
+
+    [Header("Movement Settings")]
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 5.0f;
+
+    private Vector2 moveInput;
+    public bool isGrounded;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        controls = new PlayerControls();
+        rb.freezeRotation = true; // Keep player upright, can toggle in inspector
+
+        // Movement InputSystem
+        controls.Movement.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Movement.Move.canceled += ctx => moveInput = Vector2.zero;
+        controls.Movement.Jump.performed += ctx => Jump();
+
+    }
+
+    private void OnEnable()
+    {
+        controls.Movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Movement.Disable();
+    }
+
+    private void Update()
+    {
+        // Horizontal movement
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
+
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Remember to tag scene objects "Ground" as needed
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = false;
+    }
+
+}
