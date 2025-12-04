35c1d9072aa1a80fb0c3eec1616428108c79182e
BryanJD9 <156015169+BryanJD9@users.noreply.github.com>
2025-12-04 07:14:56 -0500

    Fixed boss testing code. added level 2 button to mainmenu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,6 +6,7 @@ public class MainMenu : MonoBehaviour
 {
     [Header("UI Buttons")]
     [SerializeField] private Button level1Button;
+    [SerializeField] private Button level2Button;
     [SerializeField] private Button bossLevelButton;
 
     private void Start()
@@ -14,6 +15,9 @@ public class MainMenu : MonoBehaviour
         if (level1Button != null)
             level1Button.onClick.AddListener(LoadLevel1);
 
+        if (level2Button != null)
+            level2Button.onClick.AddListener(LoadLevel2);
+
         if (bossLevelButton != null)
             bossLevelButton.onClick.AddListener(LoadBossLevel);
     }
@@ -21,6 +25,10 @@ public class MainMenu : MonoBehaviour
     private void LoadLevel1()
     {
         SceneManager.LoadScene("Level1");
+    }
+    private void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2_Vertical");
     }
 
     private void LoadBossLevel()
