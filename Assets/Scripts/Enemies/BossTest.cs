d10c2290e9d99a3271e37b28d0eda7e2bc522a43
BryanJD9 <156015169+BryanJD9@users.noreply.github.com>
2025-12-04 01:15:36 -0500

    Fixed boss contact damage to player
    This reverts commit ef107179c22a45d308f8697a064a95d1b5d0b2cb.

--- Assets/Scripts/Enemies/BossTest.cs
+++ Assets/Scripts/Enemies/BossTest.cs
@@ -164,6 +164,15 @@ public class BossTest : Enemy
         base.Die(); // destroys the boss object
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+            if (player != null)
+                player.TakeDamage(1);
+        }
+    }
 
 
 }
